e67165bec5ca07035f27c84f7bbc4cf7909e189f
anula <anula@users.noreply.github.com>
2023-12-01 21:07:06 +0100

    Make the new day script a bit nicer

--- start_new_day.py
+++ start_new_day.py
@@ -7,9 +7,11 @@ import shutil
 parser = argparse.ArgumentParser(
   prog='NewDayCreator',
   description='Create new day for advent of code')
-parser.add_argument('-d', '--day_number', type=int,
-                    required=True,
+parser.add_argument('day_number', type=int,
                     help='The number of the day to create.')
+parser.add_argument('problem_name', type=str,
+                    default=None, nargs='?',
+                    help='How to name code files.')
 parser.add_argument('-ct', '--code_template', type=str,
                     default='rust_template.rs',
                     help='File with code template to copy over.')
@@ -22,11 +24,14 @@ def main():
 
   day_name = f'day{args.day_number}'
   if os.path.isdir(day_name):
-    print(f'Directory "{day_name}" already exists, stopping...')
+    print(f'Directory "{day_name}" already exists, aborting...')
     return
 
   os.mkdir(day_name)
-  code_path = os.path.join(day_name, f'{day_name}.rs')
+  if args.problem_name is not None:
+    code_path = os.path.join(day_name, f'{args.problem_name}.rs')
+  else:
+    code_path = os.path.join(day_name, f'{day_name}.rs')
   shutil.copyfile(args.code_template, code_path)
   makefile_path = os.path.join(day_name, 'Makefile')
   shutil.copyfile(args.makefile_template, makefile_path)
